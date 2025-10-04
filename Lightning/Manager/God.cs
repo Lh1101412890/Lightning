b89d1517fdeda0187485ca4c359b4d29cae3ec93
鸿 李 <1101412890@qq.com>
2025-10-04 14:38:18 +0800

    代码优化

--- Lightning/Manager/God.cs
+++ Lightning/Manager/God.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Xml;
 
-using Lightning.Extension;
 using Lightning.Information;
 
 using Microsoft.Win32;
